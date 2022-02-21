184cba1df8ea5e67af29280d0f68ba0bd1d0bf5c
kimerser <58280328+kimerser@users.noreply.github.com>
2022-02-21 22:03:47 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-import cv2
+from cv2 import cv2
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    print(cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml"))
     return "Hello MyProJect RMUTI!"
 
 if __name__ == "__main__":
