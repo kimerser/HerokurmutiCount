7876d807c59fde8b5323cdb6e97e7febf2a1264a
kimerser <58280328+kimerser@users.noreply.github.com>
2022-02-22 00:51:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import cv2
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(1)  # use 0 for web camera
+print(cv2.getThreadNum())
+camera =  cv2.VideoCapture()
+# use 0 for web camera
 #  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
 # for local webcam use cv2.VideoCapture(0)
 
