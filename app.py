b8b5445ba4e2265203547533bbaf1cd756a91b4f
kimerser <58280328+kimerser@users.noreply.github.com>
2022-02-21 18:57:22 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from  flask import Flask
+# from  flask import Flask
+#
+# app = Flask(__name__)
+#
+# @app.route("/")
+# def index():
+#     return "Hello MyProJect RMUTI!"
+#
+# if __name__ == "__main__":
+#     app.run()
 
+from flask import Flask, Response
+import cv2
 app = Flask(__name__)
-
-@app.route("/")
+video = cv2.VideoCapture(0)
+@app.route('/')
 def index():
-    return "Hello MyProJect RMUTI!"
-
+    return "Default Message"
+def gen(video):
+    while True:
+        success, image = video.read()
+        ret, jpeg = cv2.imencode('.jpg', image)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+@app.route('/video_feed')
+def video_feed():
+    global video
+    return Response(gen(video),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == "__main__":
     app.run()
