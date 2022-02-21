69063a799c3a796f07736ea7580e4b0f1b784be7
kimerser <58280328+kimerser@users.noreply.github.com>
2022-02-21 23:02:41 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask
-from cv2 import cv2
-
+#Import necessary libraries
+from flask import Flask, render_template, Response
+import cv2
+#Initialize the Flask app
 app = Flask(__name__)
-@app.route("/")
-def index():
-    return "Hello MyProJect RMUTI!"
-
-if __name__ == "__main__":
-    app.run()
