22d329c4ce3c7010d3e5425d1ec65745e1de9e7d
kimerser <58280328+kimerser@users.noreply.github.com>
2022-02-21 23:10:47 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -3,3 +3,30 @@ from flask import Flask, render_template, Response
 import cv2
 #Initialize the Flask app
 app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+'''
+for ip camera use - rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' 
+for local webcam use cv2.VideoCapture(0)
+'''
+@app.route('/')
+def index():
+    return "hi";
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+if __name__ == "__main__":
+    app.run()
