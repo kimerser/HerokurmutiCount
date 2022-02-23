6e058a09e1ea46426ed4dd0fe99ec4c54d08ee81
kimerser <58280328+kimerser@users.noreply.github.com>
2022-02-23 22:52:54 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
+#Import necessary libraries
 from flask import Flask, render_template, Response
 import cv2
-
+#Initialize the Flask app
 app = Flask(__name__)
 
-camera = cv2.VideoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')  # use 0 for web camera
-#  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
-# for local webcam use cv2.VideoCapture(0)
 
-def gen_frames():  # generate frame by frame from camera
+all_camera_idx_available = []
+
+for camera_idx in range(10):
+    camera = cv2.VideoCapture(camera_idx)
+    if camera.isOpened():
+        print(f'Camera index available: {camera_idx}')
+        all_camera_idx_available.append(camera_idx)
+        camera.release()
+
+# camera = cv2.VideoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')
+'''
+for ip camera use - rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' 
+for local webcam use cv2.VideoCapture(0)
+'''
+
+def gen_frames():
     while True:
-        # Capture frame-by-frame
         success, frame = camera.read()  # read the camera frame
         if not success:
             break
@@ -19,18 +31,13 @@ def gen_frames():  # generate frame by frame from camera
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
-@app.route('/')
-def index():
-    """Video streaming home page."""
-    return render_template('index.html')
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
