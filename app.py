3d055c1a618576908154d8d6e3db8017ccfbb7c8
kimerser <58280328+kimerser@users.noreply.github.com>
2022-02-23 21:17:16 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,40 @@
 from flask import Flask, render_template, Response
 import cv2
-import socket
-import io
+
 app = Flask(__name__)
-vc = cv2.VideoCapture(0)
-@app.route('/')
-def index():
-   """Video streaming ."""
-   return render_template('index.html')
-def gen():
-   """Video streaming generator function."""
-   while True:
-       rval, frame = vc.read()
-       cv2.imwrite('pic.jpg', frame)
-       yield (b'--frame\r\n' 
-              b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n')
+
+
+camera =  cv2.VideoCapture(0)
+
+# use 0 for web camera
+#  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
+# for local webcam use cv2.VideoCapture(0)
+
+def gen_frames():  # generate frame by frame from camera
+    while True:
+        # Capture frame-by-frame
+
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
 @app.route('/video_feed')
 def video_feed():
-   """Video streaming route. Put this in the src attribute of an img tag."""
-   return Response(gen(),
-                   mimetype='multipart/x-mixed-replace; boundary=frame')
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/')
+def index():
+    print(cv2.getThreadNum())
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    app.run(debug=True)
