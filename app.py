2066652d79cd10fae405cb359afec7c2863ceee7
kimerser <58280328+kimerser@users.noreply.github.com>
2022-02-24 22:07:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(debug=True,thread=True)
+    app.run()
