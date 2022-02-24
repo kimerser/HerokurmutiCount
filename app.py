169f5ae7f8680ee0f16133760b83f43fc8f99a85
kimerser <58280328+kimerser@users.noreply.github.com>
2022-02-24 22:27:39 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,39 +1,21 @@
-#Import necessary libraries
-from flask import Flask, render_template, Response
+from flask import Flask, Response
 import cv2
-import os
-#Initialize the Flask app
 app = Flask(__name__)
-camera = cv2.VideoCapture('rtsp://admin:123456@192.168.1.216/H264?ch=1&subtype=0')
-
-#if os.environ.get('WERKZEUG_RUN_MAIN') or Flask.debug is False:
-#    camera = cv2.VideoCapture(0)
-
-# camera = cv2.VideoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')
-'''
-for ip camera use - rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' 
-for local webcam use cv2.VideoCapture(0)
-'''
-
-def gen_frames():
-    #camera = cv2.VideoCapture(0)
-    while True:
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
+video = cv2.VideoCapture(0)
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return "Default Message"
+def gen(video):
+    while True:
+        success, image = video.read()
+        ret, jpeg = cv2.imencode('.jpg', image)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == "__main__":
-    app.run()
+    global video
+    return Response(gen(video),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__ == '__main__':
+    app.run(host='countingstuden.herokuapp.com', threaded=True)
