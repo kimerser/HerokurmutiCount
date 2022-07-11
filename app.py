ba310746cc7a9a5de28c9ad9a16c931cebd6d924
kimerser <kimmer_09432@Outlook.com>
2022-07-11 10:43:23 +0700

    addbeforeTest

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from datetime import datetime
 from typing import Type
-from flask import Flask, render_template, redirect, request, url_for, Response, jsonify, flash, session
+from flask import Flask, render_template, redirect, request, url_for, Response, jsonify, flash
 import psycopg2
-from werkzeug.security import generate_password_hash, check_password_hash
+# from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 
-# mydb = mysql.connector.connect(
-#     host="ec2-3-224-125-117.compute-1.amazonaws.com",
-#     user="lkpgoucsyezcgn",
-#     password="3ddf4e897e1859e61bfffa3bf668acf9633c6dd82c7f03aec8308be5234ecf3e",
-#     database="d4pf0p93fbnod2"
-# )
 mydb = psycopg2.connect(database="d7s6m6ath6ve13"
     , user="zgvqfwsigmwxmz"
     , password="1165a95e7ba1d4c6455d6cab658649c2a5490c8fff39ab89d0be207935eb351c"
@@ -20,8 +14,6 @@ mycursor = mydb.cursor()
 
 @app.route("/index")
 def index():
-    # con=mydb.connection.cursor()
-    # sql = "SELECT * FROM faculty order by fac_id "
     sql = "select dc.fac_id ,f.fac_name,f.fac_name,dc.num_of_graduates,dc.dateuse,dc.range_count,dc.degree_id ,dc.count_id,dc.count_no from date_counts dc  inner join faculty f on dc.fac_id = f.fac_id inner join degrees d on d.degree_id = dc.degree_id order by dc.dateuse, dc.count_no "
     mycursor.execute(sql)
     res = mycursor.fetchall()
@@ -38,13 +30,14 @@ def index():
     mycursor.execute(sql)
     facsum = mycursor.fetchall()
 
-    sql = 'select p."timeDelay"  ,p.left ,p.right,p.timenotify  from parameters p'
+    sql = 'select p."timeDelay"  ,p.left ,p.right,p.timenotify , p.range_count  from parameters p'
     mycursor.execute(sql)
     side = mycursor.fetchall()
     # print(rows)
-
-    
-    return render_template("index.html", datas=res,facdetail=facdetail, datasert=datasert, facsum=facsum, side=side)
+    for side in side:
+        # dateuse = datamonitor[0]
+        rangeCount = side[4]
+    return render_template("index.html", datas=res,facdetail=facdetail, datasert=datasert, facsum=facsum, side=side,rangeCount=rangeCount)
 
 @app.route("/configparamers")
 def configparamers():
@@ -54,17 +47,13 @@ def configparamers():
     print(side)
     return render_template("admin.html", side=side)
 
-# @app.route("/login")
-# def login():
-#     # sql = 'select p."timeDelay"  ,p.left ,p.right,p.timenotify,p.dateuse,p.range_count   from parameters p'
-#     # mycursor.execute(sql)
-#     # side = mycursor.fetchall()
-#     # print(side)
-#     return render_template("login.html")
 
 @app.route("/monitoring", methods=['GET'])
 def monitoring():
-    
+    currentPerson = 0
+    numOfGraduates = 0
+    percen = 0
+    balance = 0
     print(datetime.now())
     now = datetime.now()
     current_date = now.strftime("%Y-%m-%d")
@@ -72,7 +61,7 @@ def monitoring():
     endTime = now
     print(current_date)
     val = [current_date]
-    selectDate = "select cp.start_time ,cp.end_time from count_proc cp left join date_counts dc ON cp.count_id  =  dc.count_id  WHERE dc.dateuse  =  %s   and cp.start_time is not null order by  dc.count_no  desc LIMIT 1  "
+    selectDate = "select cp.start_time ,cp.end_time from count_proc cp left join date_counts dc ON cp.count_id  =  dc.count_id  WHERE dc.dateuse  =  %s   and cp.start_time is not null  and dc.range_count  = '1'   order by  dc.count_no  desc LIMIT 1  "
     mycursor.execute(selectDate,val)
     selectDate = mycursor.fetchall()
     for selectDate in selectDate:
@@ -88,7 +77,7 @@ def monitoring():
         endTime = now.strftime("%H:%M:%S")
     else:
         endTime = endTime.strftime("%H:%M:%S")
-    sql = "select dc.dateuse, sum(cp.current_person),sum(dc.num_of_graduates)  from count_proc cp inner join date_counts dc ON cp.count_id = dc.count_id where  dc.dateuse =  %s  group by dc.dateuse"
+    sql = "select dc.dateuse, sum(cp.current_person),sum(dc.num_of_graduates)  from count_proc cp inner join date_counts dc ON cp.count_id = dc.count_id where  dc.dateuse =  %s  and dc.range_count  = '1'  group by dc.dateuse"
     mycursor.execute(sql,val)
     datamonitor = mycursor.fetchall()
     print(datamonitor)
@@ -99,11 +88,182 @@ def monitoring():
 
     print(currentPerson , " currentPerson")
     print(numOfGraduates , " numOfGraduates")
-    percen = (currentPerson/numOfGraduates) * 100 
-    percen = int(percen)
-    print(percen)
-    balance = numOfGraduates - currentPerson
-    return render_template("monitoring.html",percen = percen, currentPerson = currentPerson, balance = balance ,startTime=startTime , endTime =endTime)
+    if(currentPerson != 0 and numOfGraduates != 0):
+        percen = (currentPerson/numOfGraduates) * 100 
+        percen = int(percen)
+        print(percen)
+        balance = numOfGraduates - currentPerson
+
+    currentPerson2 = 0
+    numOfGraduates2 = 0
+    percen2 = 0
+    balance2 = 0
+    current_date2 = now.strftime("%Y-%m-%d")
+    startTime2 = now
+    endTime2 = now
+    print(current_date2)
+    val = [current_date]
+    selectDate2 = "select cp.start_time ,cp.end_time from count_proc cp left join date_counts dc ON cp.count_id  =  dc.count_id  WHERE dc.dateuse  =  %s   and cp.start_time is not null  and dc.range_count  = '2'   order by  dc.count_no  desc LIMIT 1  "
+    mycursor.execute(selectDate2,val)
+    selectDate2 = mycursor.fetchall()
+    for selectDate2 in selectDate2:
+        # dateuse = datamonitor[0]
+        startTime2 = selectDate2[0]
+        endTime2 = selectDate2[1]
+    if startTime2 is None :
+        startTime2 = now.strftime("%H:%M:%S")
+    else:
+         startTime2 = startTime2.strftime("%H:%M:%S")
+   
+    if endTime2 is None :
+        endTime2 = now.strftime("%H:%M:%S")
+    else:
+        endTime2 = endTime2.strftime("%H:%M:%S")
+    sql = "select dc.dateuse, sum(cp.current_person),sum(dc.num_of_graduates)  from count_proc cp inner join date_counts dc ON cp.count_id = dc.count_id where  dc.dateuse =  %s  and dc.range_count  = '2'  group by dc.dateuse"
+    mycursor.execute(sql,val)
+    datamonitor2 = mycursor.fetchall()
+    print(datamonitor2)
+    for datamonitor2 in datamonitor2:
+        # dateuse = datamonitor[0]
+        currentPerson2 = datamonitor2[1]
+        numOfGraduates2 = datamonitor2[2]
+    if(currentPerson2 != 0 or numOfGraduates2 != 0):
+        print(currentPerson2 , " currentPerson2")
+        print(numOfGraduates2 , " numOfGraduates2")
+        percen2 = (currentPerson2/numOfGraduates2) * 100 
+        percen2 = int(percen2)
+        print(percen2)
+        balance2 = numOfGraduates2 - currentPerson2
+
+    currentPerson3 = 0
+    numOfGraduates3 = 0
+    percen3 = 0
+    balance3 = 0
+    current_date3 = now.strftime("%Y-%m-%d")
+    startTime3 = now
+    endTime3 = now
+    print(current_date3)
+    val = [current_date]
+    selectDate3 = "select cp.start_time ,cp.end_time from count_proc cp left join date_counts dc ON cp.count_id  =  dc.count_id  WHERE dc.dateuse  =  %s   and cp.start_time is not null  and dc.range_count  = '3'   order by  dc.count_no  desc LIMIT 1  "
+    mycursor.execute(selectDate3,val)
+    selectDate3 = mycursor.fetchall()
+    for selectDate3 in selectDate3:
+        # dateuse = datamonitor[0]
+        startTime3 = selectDate3[0]
+        endTime3 = selectDate3[1]
+    if startTime3 is None :
+        startTime3 = now.strftime("%H:%M:%S")
+    else:
+         startTime3 = startTime3.strftime("%H:%M:%S")
+   
+    if endTime3 is None :
+        endTime3 = now.strftime("%H:%M:%S")
+    else:
+        endTime3 = endTime3.strftime("%H:%M:%S")
+    sql = "select dc.dateuse, sum(cp.current_person),sum(dc.num_of_graduates)  from count_proc cp inner join date_counts dc ON cp.count_id = dc.count_id where  dc.dateuse =  %s  and dc.range_count  = '3'  group by dc.dateuse"
+    mycursor.execute(sql,val)
+    datamonitor3 = mycursor.fetchall()
+    print(datamonitor3)
+    for datamonitor3 in datamonitor3:
+        # dateuse = datamonitor[0]
+        currentPerson3 = datamonitor3[1]
+        numOfGraduates3 = datamonitor3[2]
+    if(currentPerson3 != 0 or numOfGraduates3 != 0):
+        print(currentPerson3 , " currentPerson3")
+        print(currentPerson3 , " numOfGraduates3")
+        percen3 = (currentPerson3/numOfGraduates3) * 100 
+        percen3 = int(percen3)
+        print(percen3)
+        balance3 = numOfGraduates3 - currentPerson3
+
+
+    currentPerson4 = 0
+    numOfGraduates4 = 0
+    percen4 = 0
+    balance4 = 0
+    current_date4 = now.strftime("%Y-%m-%d")
+    startTime4 = now
+    endTime4 = now
+    print(current_date4)
+    val = [current_date]
+    selectDate4 = "select cp.start_time ,cp.end_time from count_proc cp left join date_counts dc ON cp.count_id  =  dc.count_id  WHERE dc.dateuse  =  %s   and cp.start_time is not null  and dc.range_count  = '4'   order by  dc.count_no  desc LIMIT 1  "
+    mycursor.execute(selectDate4,val)
+    selectDate4 = mycursor.fetchall()
+    for selectDate4 in selectDate4:
+        # dateuse = datamonitor[0]
+        startTime4 = selectDate4[0]
+        endTime4 = selectDate4[1]
+    if startTime4 is None :
+        startTime4 = now.strftime("%H:%M:%S")
+    else:
+         startTime4 = startTime4.strftime("%H:%M:%S")
+   
+    if endTime4 is None :
+        endTime4 = now.strftime("%H:%M:%S")
+    else:
+        endTime4 = endTime4.strftime("%H:%M:%S")
+    sql = "select dc.dateuse, sum(cp.current_person),sum(dc.num_of_graduates)  from count_proc cp inner join date_counts dc ON cp.count_id = dc.count_id where  dc.dateuse =  %s  and dc.range_count  = '4'  group by dc.dateuse"
+    mycursor.execute(sql,val)
+    datamonitor4 = mycursor.fetchall()
+    print(datamonitor4)
+    for datamonitor4 in datamonitor4:
+        # dateuse = datamonitor[0]
+        currentPerson4 = datamonitor4[1]
+        numOfGraduates4 = datamonitor4[2]
+    if(currentPerson4 != 0 or numOfGraduates4 != 0):
+        print(currentPerson4 , " currentPerson4")
+        print(currentPerson4 , " numOfGraduates4")
+        percen4 = (currentPerson4/numOfGraduates4) * 100 
+        percen4 = int(percen4)
+        print(percen4)
+        balance4 = numOfGraduates4 - currentPerson4
+    
+    currentPersonall = 0
+    numOfGraduatesall = 0
+    percenall = 0
+    balanceall = 0
+    current_dateall = now.strftime("%Y-%m-%d")
+    startTimeall = now
+    endTimeall = now
+    print(current_dateall)
+    val = [current_date]
+    selectDateall = "select cp.start_time ,cp.end_time from count_proc cp left join date_counts dc ON cp.count_id  =  dc.count_id  WHERE dc.dateuse  =  %s   and cp.start_time is not null   order by  dc.count_no  desc LIMIT 1  "
+    mycursor.execute(selectDateall,val)
+    selectDateall = mycursor.fetchall()
+    for selectDateall in selectDateall:
+        # dateuse = datamonitor[0]
+        startTimeall = selectDateall[0]
+        endTimeall = selectDateall[1]
+    if startTimeall is None :
+        startTimeall = now.strftime("%H:%M:%S")
+    else:
+         startTimeall = startTimeall.strftime("%H:%M:%S")
+   
+    if endTimeall is None :
+        endTimeall = now.strftime("%H:%M:%S")
+    else:
+        endTimeall = endTimeall.strftime("%H:%M:%S")
+    sql = "select dc.dateuse, sum(cp.current_person),sum(dc.num_of_graduates)  from count_proc cp inner join date_counts dc ON cp.count_id = dc.count_id where  dc.dateuse =  %s    group by dc.dateuse"
+    mycursor.execute(sql,val)
+    datamonitorall = mycursor.fetchall()
+    print(datamonitorall)
+    for datamonitorall in datamonitorall:
+        # dateuse = datamonitor[0]
+        currentPersonall = datamonitorall[1]
+        numOfGraduatesall = datamonitorall[2]
+    if(currentPersonall != 0 or numOfGraduatesall != 0):
+        print(currentPersonall , " currentPerson4")
+        print(currentPersonall , " numOfGraduates4")
+        percenall = (currentPersonall/numOfGraduatesall) * 100 
+        percenall = int(percenall)
+        print(percenall)
+        balanceall = numOfGraduatesall - currentPersonall
+
+    return render_template("monitoring.html",percen = percen, currentPerson = currentPerson, balance = balance ,startTime=startTime , endTime =endTime ,
+    percen2 = percen2, currentPerson2 = currentPerson2, balance2 = balance2 ,startTime2=startTime2 , endTime2 =endTime2,
+    percen3 = percen3, currentPerson3 = currentPerson3, balance3 = balance3 ,startTime3=startTime3 , endTime3 =endTime3,
+    percen4 = percen4, currentPerson4 = currentPerson4, balance4 = balance4 ,startTime4=startTime4, endTime4 =endTime4,
+    percenall = percenall, currentPersonall = currentPersonall, balanceall = balanceall ,startTimeall=startTimeall, endTimeall =endTimeall)
 
 
 
@@ -192,6 +352,15 @@ def find_fac():
     val =[dateuse]
     mycursor.execute(sql,val)
     facsum = mycursor.fetchall()
+
+
+    sql = 'select p."timeDelay"  ,p.left ,p.right,p.timenotify , p.range_count  from parameters p'
+    mycursor.execute(sql)
+    side = mycursor.fetchall()
+    # print(rows)
+    for side in side:
+        # dateuse = datamonitor[0]
+        rangeCount = side[4]
     # # print(res)
     # sql = "SELECT sum(num_of_graduates) FROM faculty"
     # mycursor.execute(sql)
@@ -201,7 +370,7 @@ def find_fac():
     # mycursor.execute(sql)
     # side = mycursor.fetchall()
     # # print(rows)
-    return render_template("index.html", datas=res,facdetail=facdetail,datasert=datasert,facsum= facsum)
+    return render_template("index.html", datas=res,facdetail=facdetail,datasert=datasert,facsum= facsum,rangeCount =rangeCount)
 
 
 @ app.route("/update_right", methods=['GET', 'POST'])
@@ -227,12 +396,21 @@ def linetoken():
 @ app.route("/delaytime", methods=['GET', 'POST'])
 def delaytime():
     delaytime = request.form["delaytime"]
-    # sql = "UPDATE `parameter` SET `timeDelay`= %s"
-    sql = 'UPDATE parameters SET "timeDelay"= %s'
-    val = [delaytime]
-    mycursor.execute(sql, val)
-    mydb.commit()
-    return redirect(url_for("configparamers"))
+    if(delaytime is None or delaytime == '' ):
+        if(type(delaytime) == str):
+            print("delaytime")
+            flash('ค่าเวลาที่กำหนด ไม่ถูกต้อง')
+        else:
+            print("delaytime")
+            flash('ค่าเวลาที่กำหนดไม่ใช่ตัวเลข')
+        return redirect(url_for("configparamers"))
+    else:
+        # sql = "UPDATE `parameter` SET `timeDelay`= %s"
+        sql = 'UPDATE parameters SET "timeDelay"= %s'
+        val = [delaytime]
+        mycursor.execute(sql, val)
+        mydb.commit()
+        return redirect(url_for("configparamers"))
 
 @ app.route("/update_timenotify", methods=['GET', 'POST'])
 def timenotify():
@@ -434,11 +612,11 @@ def delelte(id_data):
 
 @app.route('/')
 def home():
-    # Check if user is loggedin
-    if 'loggedin' in session:
-        # User is loggedin show them the home page
-        return render_template('login.html', username=session['username'])
-        #  User is not loggedin redirect to login page
+    # # Check if user is loggedin
+    # if 'loggedin' in session:
+    #     # User is loggedin show them the home page
+    #     return render_template('login.html', username=session['username'])
+    #     #  User is not loggedin redirect to login page
     return redirect(url_for('login'))
 
 from datetime import datetime
@@ -482,15 +660,16 @@ def login():
                     return redirect(url_for('index'))
             else:
                 # Account doesnt exist or username/password incorrect
-                flash('Incorrect username/password')
+                flash('username หรือ password ไม่ถูกต้อง')
         else:
             # Account doesnt exist or username/password incorrect
-            flash('Incorrect username/password')
+            flash('username หรือ password ไม่ถูกต้อง')
 
     return render_template('login.html')
 
 
 if __name__ == "__main__":
+    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
     app.run(debug=True)
 
 
